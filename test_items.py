739cb4b9c6c7683f55a23fd2ec3d99c89063b660
valery.degtyar <degtyar060589@gmail.com>
2023-01-05 01:18:51 +0300

    update1

--- test_items.py
+++ test_items.py
@@ -4,7 +4,7 @@ import time
 link = ("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
 
 
-def test_find_buttun_busket(browser):
+def test_find_button_busket(browser):
     browser.get(link)
     time.sleep(30)
     x = len(browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket"))
